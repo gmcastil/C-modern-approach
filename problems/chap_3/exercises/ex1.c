3ea788a41cc3c46c0fb50ca588a7934455f3065f
George Castillo <gmcastil@gmail.com>
2016-04-17 16:58:00 -0600

    Started working on chapter 3.

---
+++ problems/chap_3/exercises/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("[%6d, %4d]\n", 86, 1040);
+  printf("[    86, 1040]\n");
+  printf("[%12.5e]\n", 30.253);
+  printf("[ 3.02530e+01]\n");
+  printf("[%.4f]\n", 83.162);
+  printf("[83.1620]\n");
+  printf("[%-6.2g]\n", .0000009979);
+  printf("[1e-6 ]\n");
+}
