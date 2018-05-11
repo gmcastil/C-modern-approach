c252f3794b01fb0388fff89fd0d68c317e50eea0
George Castillo <gmcastil@gmail.com>
2018-05-10 23:11:49 -0600

    Added one of the first projects in chapter 7.

---
+++ problems/chap_7/projects/sum2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+        long n;
+        long sum = 0;
+
+        printf("This program prints a series of integers.\n");
+        printf("Enter integers (0 to terminate): ");
+
+        scanf("%ld", &n);
+        while (n != 0) {
+                sum += n;
+                scanf("%ld", &n);
+        }
+        printf("The sum is: %ld\n", sum);
+
+        return 0;
+}
